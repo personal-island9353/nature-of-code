06d2e705845c31790ad41dd31c4b91578c7e8c22
personal.island9353 <personal.island9353@gmail.com>
2025-10-26 00:02:25 +0000

    Fix scaffolding

--- src/index.js
+++
@@ -1,8 +0,0 @@
-function setup() {
-  p5.createCanvas(400, 400);
-}
-function draw() {
-  p5.background(220);
-}
-export {};
-//# sourceMappingURL=index.js.map
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from "vite";
 
 export default defineConfig(({ mode: _mode }) => {
   return {
+    root: "src",
+    build: {
+      outDir: "../dist",
+    },
     server: {
       port: 5173,
       host: "127.0.0.1",
